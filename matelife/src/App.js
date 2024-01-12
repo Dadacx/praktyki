5792a0a5972237f5831a8c45b51bb794bfb5c282
Dadacx <84072588+Dadacx@users.noreply.github.com>
2024-01-12 16:37:59 +0100

    menu wyboru trybów

--- matelife/src/App.js
+++ matelife/src/App.js
@@ -15,7 +15,6 @@ const Menu = () => {
   return (
         <div className="menu_items">
           <img className="matelife" alt="matelife_logo" src={matelife}></img>
-          
           <div className="play btn underline-animation">graj</div>
           <div className="skins btn underline-animation">skórki</div>
           <div className="settings btn underline-animation">ustawienia</div>
@@ -26,14 +25,23 @@ const Mode = () => {
   return (
         <div className="mode_items">
           <div className="select_mode">wybierz tryb</div>
-          <div className="mode_box">
-            <div>dodawanie</div>
-            <div>1-25</div>
-            </div>
-          <div className="skins btn underline-animation">skógdfgdgsdgrki</div>
-          <div className="settings btn underline-animation">ustawdsgsdgdgdgdgienia</div>
+          <div className="boxes">
+          <ModeBox text1="dodawanie" text2="odejmowanie" przedzial="1-25" margin="150px" />
+          <ModeBox text1="mnożenie" text2="dzielenie" przedzial="1-25" margin="47px" />
+          <ModeBox text1="dodawanie" text2="odejmowanie" przedzial="1-100" margin="125px" />
+          <ModeBox text1="mnożenie" text2="dzielenie" przedzial="1-100" margin="7px" />
+          </div>
         </div>
   );
+}
+const ModeBox = (props) => {
+  return (
+    <div className="mode_box" style={{marginTop:props.margin}}>
+      <div>{props.text1}</div>
+      <div>{props.text2}</div>
+      <div>{props.przedzial}</div>
+    </div>
+  )
 }
 
 export default App;
